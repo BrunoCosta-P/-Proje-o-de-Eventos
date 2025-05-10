b259f9d3b17e85e26c09783c6a3ba3e0fb94174b
Bruno Costa <80495655+BrunoCosta-P@users.noreply.github.com>
2025-05-10 18:58:21 -0300

    feat: criando estrutura para recuperar dados mocados

---
+++ src/app/features/event-projection/models/event.model.ts
@@ -0,0 +1,30 @@
+export interface EventActivity {
+  meetings: number;
+  emails: number;
+  calls: number;
+  follows: number;
+}
+
+export interface DailyEvent {
+  day: number;
+  events: EventActivity;
+}
+
+export type Priority = 'HIGH' | 'MEDIUM' | 'LOW';
+
+export interface CycleActivityDetail extends EventActivity {
+  day: number;
+}
+
+export interface Cycle {
+  name: string;
+  availableEntities: number;
+  priority: Priority;
+  structure: CycleActivityDetail[];
+  selected?: boolean;
+}
+
+export interface EventsProjectionData {
+  eventsProjection: DailyEvent[];
+  cycles: Cycle[];
+}
